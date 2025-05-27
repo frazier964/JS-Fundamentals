ad455d07c7086a04bff26c09f24f1ee22d79dd30
frazier964 <hillarybella2@gmail.com>
2025-05-27 17:57:04 +0300

    8-square.js

---
+++ 8-square.js
@@ -0,0 +1,16 @@
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+ if (size <= 0) {
+ } else {
+ for (let i = 0; i < size; i++) {
+      let row = '';
+for (let j = 0; j < size; j++) {
+        row += 'x';
+      }
+ console.log(row);
+    }
+  }
+}
+
